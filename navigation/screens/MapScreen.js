98ec51163ebf571846710ae698834653368adc7e
Mylanos <marekziska@Blablabla2.local>
2022-04-24 20:47:38 +0200

    minor changes

--- navigation/screens/MapScreen.js
+++ navigation/screens/MapScreen.js
@@ -1,9 +1,9 @@
 import * as React from "react";
 import { View, Text} from 'react-native-ui-lib';
 import { StyleSheet } from 'react-native';
-import MapView from "react-native-maps";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
 
-var markers = [{
+const markers = [{
     latitude: 47.40735,
     longitude: 9.73132,
     latitudeDelta: 0.01,
@@ -25,18 +25,67 @@ var markers = [{
     name: "Cafesito"
   }];
 
+
 const MapScreen = ({ navigation }) => {
+  
+  const getInitialState = () => {
+    return {
+        latitude: 47.41235,
+        longitude: 9.74324,
+        latitudeDelta: 0.06,
+        longitudeDelta: 0.02,
+      };
+  }
+
+  const [region, setRegion] = React.useState(getInitialState())
+
+  const onRegionChange = (region) => {
+    setRegion({region});
+  }
+
+  const componentDidMount = () => {
+    Geolocation.getCurrentPosition(
+        (position) => {
+            console.warn(position.coords.latitude);
+            console.warn(position.coords.longitude);
+            setRegion({
+                region: {
+                    latitude: position.coords.latitude,
+                    longitude: position.coords.longitude,
+                    latitudeDelta: 0.02,
+                    longitudeDelta: 0,
+                }
+            });
+        },
+        (error) => {
+            console.warn(error.code, error.message);
+        },
+        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000},
+    )
+}
+
   return (
     <View style={styles.container}>
         {/*Render our MapView*/}
+        <Text onPress={() => navigation.navigate("Dornbirn")}
+            style={{ position: "absolute",
+            top: 0,
+            left: 0,
+        fontSize: 26, fontWeight: 'bold', zIndex: 1 }}>HOME</Text>
         <MapView
+            provider={PROVIDER_GOOGLE}
             style={styles.map}
+            showsUserLocation = {true}
+            followsUserLocation = {true}
             //specify our coordinates.
             initialRegion={{
-            latitude: 47.41235,
-            longitude: 9.74324,
-            latitudeDelta: 0.06,
-            longitudeDelta: 0.02,
+              latitude: 47.41235,
+              longitude: 9.74324,
+              latitudeDelta: 0.06,
+              longitudeDelta: 0.02,
+            }}
+            onRegionChange={region => {
+              setRegion({region})
             }}
             >
             {markers.map((val, index) => {
@@ -53,8 +102,6 @@ const MapScreen = ({ navigation }) => {
              })}
   
         </MapView>
-      <Text onPress={() => navigation.navigate("Dornbirn")}
-            style={{ fontSize: 26, fontWeight: 'bold' }}>BACK</Text>
     </View>
   );
 };
@@ -63,11 +110,16 @@ const MapScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1, //the container will fill the whole screen.
-    justifyContent: "flex-end",
+    backgroundColor: '#fff',
+    justifyContent: "center",
     alignItems: "center",
   },
   map: {
-    ...StyleSheet.absoluteFillObject,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
   },
 });
 
