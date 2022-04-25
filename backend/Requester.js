022344df5ea7013df511cb68923397b919b66d64
Dominik <dro4452@students.fhv.at>
2022-04-25 13:05:37 +0200

    added datta and a smal class to call it

---
+++ backend/Requester.js
@@ -0,0 +1,13 @@
+// -searchName
+
+function getResturant(){
+    return require('./data/resturante.json')
+}
+
+function getBar(){
+    return require('./data/resturante.json')
+}
+
+function getBakery(){
+    return require('./data/resturante.json')
+}
